8032ea906036f124f6aa4a8a90ad0fe06e6f3655
one1erp <avigaile@one1.co.il>
2024-04-08 17:17:38 +0300

    Initial commit with additional files

---
+++ BGPrintCrystal/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace BGPrintCrystal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose ( );
+            }
+            base.Dispose ( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ( )
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(206, 60);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(336, 342);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(171, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "תוכנית הדפסת מכתבים";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 93);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 128);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 222);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 424);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "הדפסת מכתבים ברקע";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BGPrintCrystal/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+using System.Linq;
+using System.Windows.Forms;
+
+
+using System.Runtime.InteropServices;
+
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+using System.Diagnostics;
+using Oracle.ManagedDataAccess.Client;
+using System.Threading;
+using Timer = System.Timers.Timer;
+
+
+namespace BGPrintCrystal
+{
+    public partial class Form1 : Form
+    {
+
+        //   SdgInfo _sdgInfo = null;
+        OracleConnection oraCon = null;
+        OracleCommand cmd = null;
+        private ReportDocument cr;
+        private ConnectionInfo crConnectionInfo;
+
+        private string SdgID, wnName, COMPUTER_NAME;
+
+        //private string configPath =
+        //    @"\\vm-nautilus\nautilus-share\Extensions\BGPrintCrystal\BGPrintCrystal.exe.config";
+        OracleCommand command;
+        string logPath, server, user, password, Interval;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            //if ( Environment.MachineName == "ONE1PC1518" )
+            //{
+            //    configPath = @"C:\Work\Patholab Proj\BGPrintCrystal\BGPrintCrystal\App.config";
+            //}
+            Settings();
+            WriteLogFile("Start program");
+            String thisprocessname = Process.GetCurrentProcess().ProcessName;
+
+            if (Process.GetProcesses().Count(p => p.ProcessName == thisprocessname) > 1)
+            {
+                WriteLogFile("לא ניתן להפעיל את תוכנית ההדפסות יותר מפעם אחת!");
+                WriteLogFile("END PROGRAM");
+                this.Close();
+
+
+            }
+            listBox1.Items.Add("....... ");
+
+            var connection = GetConnection();
+            command = new OracleCommand();
+            command.Connection = connection;
+
+            cr = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+            crConnectionInfo = new ConnectionInfo();
+
+
+
+            GO();
+            timer1.Interval = int.Parse(Interval);
+            timer1.Tick += (e, r) => { GO(); };
+            timer1.Start();
+
+        }
+        bool IsProxy;
+        private void Settings()
+        {
+            var xx = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var appSettings = xx.AppSettings;
+            //var dd=bb.Settings["Log"].Value;
+            //ExeConfigurationFileMap map = new ExeConfigurationFileMap ( );
+            //map.ExeConfigFilename = System.Reflection.Assembly.GetEntryAssembly().Location;
+            //var assemblyPath = System.Reflection.Assembly.GetEntryAssembly().Location+".config";
+
+            // Configuration cfg = ConfigurationManager.OpenMappedExeConfiguration ( map, ConfigurationUserLevel.None );
+
+
+
+            //    var appSettings = cfg.AppSettings;
+
+             IsProxy = appSettings.Settings["IsProxy"].Value == "True";
+            logPath = appSettings.Settings["Log"].Value;
+            server = appSettings.Settings["Server"].Value;
+            user = appSettings.Settings["User"].Value;
+            password = appSettings.Settings["Password"].Value;
+            Interval = appSettings.Settings["Interval"].Value;
+
+        }
+
+        private void GO()
+        {
+            try
+            {
+                timer1.Stop();
+
+                string sql = string.Format(
+                    "select * from lims.BG_CRYSTAL_PRINT where WORKSTATION_NAME='{0}' and is_printed='0'",
+                    Environment.MachineName);
+                command.CommandText = sql;
+                var reader = command.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    listBox1.Items.Add("No Sdg to print.");
+
+                }
+
+                while (reader.Read())
+                {
+                    try
+                    {
+
+
+                        var id = reader["ID"].ToString();
+                        var RECORDID = reader["SDG_ID"].ToString();
+                        var rptPath = reader["PATH"].ToString();
+                        var param = reader["PARAM_NAME"].ToString();
+                        listBox1.Items.Add(RECORDID + " Is Printing");
+                        WriteLogFile(RECORDID + " Is Printing.");
+                        WriteLogFile("starting to generate letter for " + RECORDID);
+
+
+
+                        cr.Load(rptPath);
+                        crConnectionInfo.ServerName = server; // _ntlsCon.GetServerName ( );
+                        crConnectionInfo.UserID = user; // _ntlsCon.GetUsername ( );
+                        crConnectionInfo.Password = password; // _ntlsCon.GetPassword ( );
+                        cr.SetParameterValue(param, RECORDID.ToString());
+                        var CrTables = cr.Database.Tables;
+                        foreach (CrystalDecisions.CrystalReports.Engine.Table crTable in CrTables)
+                        {
+                            TableLogOnInfo crTableLoginInfo = crTable.LogOnInfo;
+                            crTableLoginInfo.ConnectionInfo = crConnectionInfo;
+                            crTable.ApplyLogOnInfo(crTableLoginInfo);
+                        }
+
+                        WriteLogFile("Sending to printer for " + RECORDID);
+
+                        cr.PrintToPrinter(1, true, 0, 0);
+                        command.CommandText = "Update lims.BG_CRYSTAL_PRINT set is_printed='1' where ID ='" + id + "'";
+                        WriteLogFile(command.CommandText);
+
+                        command.ExecuteNonQuery();
+                        listBox1.Items.Add(RECORDID + " printed successfully");
+                        WriteLogFile(RECORDID + " printed successfully");
+
+
+                    }
+
+                    catch (Exception e)
+                    {
+                        label1.Text = e.Message;
+                        listBox1.Items.Add("Error on printing " + e.Message);
+                        if (e.InnerException != null)
+                            WriteLogFile("Error on printing " + e.Message + e.InnerException.ToString());
+                        if (e.Message == "Load report failed.")
+                        {
+                            cr.Close();
+                            cr.Dispose();
+                            cr = null;
+                            cr = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+                            crConnectionInfo = new ConnectionInfo();
+                        }
+
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+                listBox1.Items.Add(ex.Message);
+                WriteLogFile(ex.Message);
+            }
+            finally
+            {
+                timer1.Start();
+
+            }
+        }
+
+        void WriteLogFile(string txt)
+        {
+            try
+            {
+
+                string dir = Path.Combine(logPath, Environment.MachineName);
+
+                string fileName = "BGPrintCrystal" + "-" + DateTime.Now.ToString("dd-MM-yyyy") + ".txt";
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                string fullPath = Path.Combine(dir, fileName);
+
+                using (FileStream file = new FileStream(fullPath, FileMode.Append, FileAccess.Write))
+                {
+                    var streamWriter = new StreamWriter(file);
+                    streamWriter.WriteLine(DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff"));
+
+                    streamWriter.WriteLine(txt);
+                    streamWriter.WriteLine();
+                    streamWriter.Close();
+                }
+            }
+            catch
+            {
+            }
+
+
+        }
+
+        public OracleConnection GetConnection()
+        {
+            OracleConnection connection = null;
+
+            //initialize variables
+            string rolecommand;
+            //try catch block
+            try
+            {
+
+                string connectionString;
+
+
+                connectionString =
+                    string.Format("Data Source={0};User ID={1};Password={2};", server, user, password);
+                //create connection
+                connection = new OracleConnection(connectionString);
+
+                //open the connection
+                connection.Open();
+
+
+            }
+            catch (Exception e)
+            {
+                //throw the exeption
+                MessageBox.Show("Err At GetConnection: " + e.Message);
+            }
+
+            return connection;
+        }
+
+
+    }
+}
